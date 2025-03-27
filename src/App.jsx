468bbe2a7036b318f33d321e6656559f7a57d28e
Noa661 <noanallet3@gmail.com>
2025-03-27 14:26:36 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      height: '100vh',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontFamily: 'sans-serif',
+      fontSize: '2rem',
+      color: '#333'
+    }}>
+      Bienvenue sur Sofa-cile 🧼✨
+    </div>
+  );
+}
